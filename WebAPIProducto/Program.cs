d4138edcc29b1e3a24a1190b591e9832c4f95d14
Thomas <calerovillalobos10@gmail.com>
2023-11-21 21:47:08 -0600

    Cadena de conexión y servicio de conexión

--- WebAPIProducto/Program.cs
+++ WebAPIProducto/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPIProducto.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Recuperar la cadena de conexión
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(connectionString)
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
